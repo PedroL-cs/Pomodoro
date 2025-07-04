23d1e8031c0f6d87f98abc6ac6f7af11457ca5c6
Pedro L. <pedrolcs30@gmail.com>
2025-07-04 02:51:17 -0300

    Criado o componente 'Heading' + Lucide

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
     return (
         <div>
-            <h1>Olá mundo</h1>
+            <Heading>
+                Olá mundo!
+                <button>
+                    <TimerIcon />
+                </button>
+            </Heading>
             <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 Nesciunt, harum quos aut soluta possimus atque fugiat ipsum nemo
