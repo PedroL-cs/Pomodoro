b9b63efcc4e4ea398fa6432f1cda9d64b741bbde
Pedro L. <pedrolcs30@gmail.com>
2025-07-04 21:26:37 -0300

    Criação do componente 'Container'

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
-    return (
-        <div>
-            <Heading>
-                Olá mundo!
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Nesciunt, harum quos aut soluta possimus atque fugiat ipsum nemo
-                architecto eaque rem, aspernatur voluptatum vero maxime modi
-                dolore in dicta unde.
-            </p>
-        </div>
-    );
+   return (
+      <>
+         <Container>
+            <Heading>LOGO</Heading>
+         </Container>
+
+         <Container>
+            <Heading>MENU</Heading>
+         </Container>
+      </>
+   );
 }
