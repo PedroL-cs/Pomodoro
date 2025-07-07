1135b2d422079b60537cdf127fe5cabd5e3f4b1e
Pedro L. <pedrolcs30@gmail.com>
2025-07-06 21:31:53 -0300

    Adiciona páginas (Sobre e 404) e cria componente de html genérico

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,25 @@
+import { Container } from '../../components/Container';
+import { GenericHtml } from '../../components/GenericHtml';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+   return (
+      <MainTemplate>
+         <Container>
+            <GenericHtml>
+               <Heading>404 - Página não encontrada </Heading>
+               <p>
+                  Opa! Parece que a página que você está tentando acessar não
+                  existe.
+               </p>
+               <p>
+                  Mas calma, você não está perdido no espaço (ainda). Dá pra
+                  voltar em segurança para a <a href='/'>página principal</a> ou{' '}
+                  <a href='/history'>para o histórico</a>.
+               </p>
+            </GenericHtml>
+         </Container>
+      </MainTemplate>
+   );
+}
