a9c12b5dfd03377c2c539a6263024adaa60791f0
Pedro L. <pedrolcs30@gmail.com>
2025-07-07 22:51:10 -0300

    Adiciona controle de input no formulário

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,17 +2,26 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useState } from 'react';
 import styles from './styles.module.css';
 
 export function MainForm() {
+   const [taskName, setTaskName] = useState('');
+
+   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+   }
+
    return (
-      <form className={styles.form} action=''>
+      <form onSubmit={handleCreateNewTask} className={styles.form} action=''>
          <div className={styles.formRow}>
             <DefaultInput
                labelText='Task'
                id='meuInput'
                type='text'
                placeholder='Digite algo'
+               value={taskName}
+               onChange={e => setTaskName(e.target.value)}
             />
          </div>
 
