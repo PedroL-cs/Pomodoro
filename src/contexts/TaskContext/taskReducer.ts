7090778e1fdb9abf19d01c6953a509e5316c7273
Pedro L. <pedrolcs30@gmail.com>
2025-07-11 19:09:46 -0300

    Adiciona Reducer

---
+++ src/contexts/TaskContext/taskReducer.ts
@@ -0,0 +1,10 @@
+import type { TaskStateModel } from '../../models/TaskStateModel';
+import { TaskActionTypes, type TaskActionModel } from './taskActions';
+
+export function taskReducer(state: TaskStateModel, action: TaskActionModel) {
+   switch (action.type) {
+      case TaskActionTypes.START_TASK:
+   }
+
+   return state;
+}
