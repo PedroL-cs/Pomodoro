583565c82a28095760c1390c7f42ba1ece88e5b4
Pedro L. <pedrolcs30@gmail.com>
2025-07-18 15:16:34 -0300

    Implementa roteamento com React-Router

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router';
 import styles from './styles.module.css';
 
 export function Footer() {
    return (
       <footer className={styles.footer}>
-         <a href=''>Entenda como funciona a técnica Pomodoro</a>
-         <a href=''>Chronos Pomodoro &copy; {new Date().getFullYear()}</a>
+         <Link to='/sobre-pomodoro/'>
+            Entenda como funciona a técnica Pomodoro
+         </Link>
+         <Link to='/'>Chronos Pomodoro &copy; {new Date().getFullYear()}</Link>
       </footer>
    );
 }
